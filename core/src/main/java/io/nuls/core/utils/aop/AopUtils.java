7932155caa0f14ccc947861039eb6bad9ca1c88f
niels1286 <niels0519@gmail.com>
2017-11-27 10:03:26 +0800

    test(aop):temp coding

--- core/src/main/java/io/nuls/core/utils/aop/AopUtils.java
+++ core/src/main/java/io/nuls/core/utils/aop/AopUtils.java
@@ -2,10 +2,6 @@ package io.nuls.core.utils.aop;
 
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
-import net.sf.cglib.proxy.MethodProxy;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
 /**
  * Created by Niels on 2017/10/13.
@@ -28,33 +24,4 @@ public class AopUtils {
         return (T) enhancer.create(paramsClass,params);
     }
 
-    public static void main(String[] args) {
-        Data dataProxy = AopUtils.createProxy(Data.class, new MethodInterceptor() {
-            @Override
-            public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
-                return proxy.invokeSuper(obj,args);
-            }
-        });
-        Data newData = new Data();
-        newData .setVal("100");
-        Field h = null;
-        try {
-            h = dataProxy.getClass().getDeclaredField("CGLIB$CALLBACK_0");
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        }
-        h.setAccessible(true);
-        try {
-           Object obj =  h.get(dataProxy);
-            System.out.println(obj);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
-        h.setAccessible(false);
-
-
-
-        System.out.println(dataProxy.getVal());
-    }
 }
