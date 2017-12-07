fd866bc9b4d7169762af40fd4756db7612cfc2c6
zoro1985 <251556768@qq.com>
2017-12-07 15:21:04 +0800

    network

--- network-impl/src/main/java/io/nuls/network/param/MainNetworkParam.java
+++ network-impl/src/main/java/io/nuls/network/param/MainNetworkParam.java
@@ -17,7 +17,7 @@ public class MainNetworkParam extends AbstractNetworkParam {
     private static MainNetworkParam instance;
 
     private MainNetworkParam() {
-        this.port = ConfigLoader.getCfgValue(NetworkConstant.NETWORK_SECTION, NetworkConstant.NETWORK_PORT, 8632);
+        this.port = ConfigLoader.getCfgValue(NetworkConstant.NETWORK_SECTION, NetworkConstant.NETWORK_SERVER_PORT, 8632);
         this.packetMagic = ConfigLoader.getCfgValue(NetworkConstant.NETWORK_SECTION, NetworkConstant.NETWORK_MAGIC, 936152748);
         this.maxInCount = ConfigLoader.getPropValue(NetworkConstant.NETWORK_PEER_MAX_IN, 20);
         this.maxOutCount = ConfigLoader.getPropValue(NetworkConstant.NETWORK_PEER_MAX_OUT, 10);
--- network/src/main/java/io/nuls/network/module/AbstractNetworkModule.java
+++ network/src/main/java/io/nuls/network/module/AbstractNetworkModule.java
@@ -3,16 +3,19 @@ package io.nuls.network.module;
 
 import io.nuls.core.constant.NulsConstant;
 import io.nuls.core.module.BaseNulsModule;
+import io.nuls.core.utils.cfg.ConfigLoader;
+import io.nuls.network.constant.NetworkConstant;
 
 /**
- *
  * @author vivi
  * @date 2017/11/21
  */
 public abstract class AbstractNetworkModule extends BaseNulsModule {
 
-    public static final short networkModuleId = 4;
-    protected AbstractNetworkModule(){
+    public static int ExternalPort;
+
+    protected AbstractNetworkModule() {
         super(NulsConstant.MODULE_ID_NETWORK);
+        ExternalPort = ConfigLoader.getCfgValue(NetworkConstant.NETWORK_SECTION, NetworkConstant.NETWORK_EXTER_PORT, 8632);
     }
 }
