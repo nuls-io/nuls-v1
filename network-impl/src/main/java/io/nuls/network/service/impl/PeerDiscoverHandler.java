dd51f40726a34640d285b323bf6dc493276eacbd
zoro1985 <251556768@qq.com>
2017-12-07 15:22:34 +0800

    network

--- network-impl/src/main/java/io/nuls/network/service/impl/PeerDiscoverHandler.java
+++ network-impl/src/main/java/io/nuls/network/service/impl/PeerDiscoverHandler.java
@@ -109,10 +109,10 @@ public class PeerDiscoverHandler implements Runnable {
 //            for (Peer peer : outPeers.getPeers()) {
 //                System.out.println("------out--ip:" + peer.getIp() + ",port:" + peer.getPort() + ",status:" + peer.getStatus());
 //            }
-//
+
             PeerGroup inPeers = peersManager.getPeerGroup(NetworkConstant.NETWORK_PEER_IN_GROUP);
             if(inPeers != null) {
-                for (Peer peer : outPeers.getPeers()) {
+                for (Peer peer : inPeers.getPeers()) {
                     System.out.println("-----in---ip:" + peer.getIp() + ",port:" + peer.getPort() + ",status:" + peer.getStatus());
                 }
             }
