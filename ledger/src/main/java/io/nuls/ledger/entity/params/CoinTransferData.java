5a426bf9f0964a9e4bfc70cf213c86577837bef7
zhouwei1985 <251556768@qq.com>
2017-12-26 20:05:59 +0800

    tx

--- ledger/src/main/java/io/nuls/ledger/entity/params/CoinTransferData.java
+++ ledger/src/main/java/io/nuls/ledger/entity/params/CoinTransferData.java
@@ -1,8 +1,6 @@
 package io.nuls.ledger.entity.params;
 
-import io.nuls.account.constant.AccountConstant;
 import io.nuls.core.chain.entity.Na;
-import io.nuls.core.context.NulsContext;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -64,30 +62,5 @@ public class CoinTransferData {
             this.toMap = new HashMap<>();
         }
         this.toMap.put(address, coin);
-    }
-
-    public static CoinTransferData getBaseUnlockCoinData(String address, Na na) {
-        CoinTransferData coinData = new CoinTransferData();
-        coinData.setFee(NulsContext.getInstance().getTxFee());
-        coinData.setCanBeUnlocked(false);
-        coinData.setUnlockHeight(0);
-        coinData.setUnlockTime(0);
-        coinData.setTotalNa(na);
-        coinData.addFrom(address, na);
-        return coinData;
-    }
-
-    public static CoinTransferData getLockHeightCoinData(String address, Na na, int unlockHeight) {
-        CoinTransferData coinData =getBaseUnlockCoinData(address, na);
-        coinData.setCanBeUnlocked(true);
-        coinData.setUnlockHeight(unlockHeight);
-        return coinData;
-    }
-
-    public static CoinTransferData getLockTimeCoinData(String address, Na na, long unlockTime) {
-        CoinTransferData coinData =getBaseUnlockCoinData(address, na);
-        coinData.setCanBeUnlocked(true);
-        coinData.setUnlockTime(unlockTime);
-        return coinData;
     }
 }
