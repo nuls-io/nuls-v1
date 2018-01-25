f70f14218089a2573a2593b4bdedc6741c639c60
niels1286 <niels0519@gmail.com>
2018-01-25 09:47:22 +0800

    consensus update

--- network-impl/src/main/java/io/nuls/network/param/DevNetworkParam.java
+++ network-impl/src/main/java/io/nuls/network/param/DevNetworkParam.java
@@ -54,14 +54,14 @@ public class DevNetworkParam extends AbstractNetworkParam {
             Log.error(e);
         }
 
-        InetSocketAddress address2 = new InetSocketAddress("192.168.1.101", port);
-        InetSocketAddress address3 = new InetSocketAddress("192.168.1.102", port);
+        InetSocketAddress address0 = new InetSocketAddress("192.168.1.103", port + 1);
         InetSocketAddress address1 = new InetSocketAddress("192.168.1.103", port);
-        InetSocketAddress address0 = new InetSocketAddress("127.0.0.1", port + 1);
-        seedNodes.add(address1);
-        seedNodes.add(address2);
-        seedNodes.add(address3);
+//        InetSocketAddress address2 = new InetSocketAddress("192.168.1.101", port);
+//        InetSocketAddress address3 = new InetSocketAddress("192.168.1.102", port);
         seedNodes.add(address0);
+        seedNodes.add(address1);
+//        seedNodes.add(address2);
+//        seedNodes.add(address3);
 
         this.messageFilter = DefaultMessageFilter.getInstance();
         this.messageHandlerFactory = DefaultNetWorkEventHandlerFactory.getInstance();
