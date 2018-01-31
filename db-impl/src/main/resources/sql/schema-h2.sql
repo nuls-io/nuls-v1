788c3ede2d858a1108e16b8b7bf2740d188f8cb3
vivi <251556768@qq.com>
2018-01-31 10:59:55 +0800

    ledger bug

--- db-impl/src/main/resources/sql/schema-h2.sql
+++ db-impl/src/main/resources/sql/schema-h2.sql
@@ -118,7 +118,7 @@ CREATE TABLE IF NOT EXISTS `tx_account_relation` (
 CREATE TABLE IF NOT EXISTS `utxo_input` (
   `tx_hash` varchar(70) NOT NULL,
   `from_index` int(5) NOT NULL,
-  `index` int(5) NOT NULL,
+  `in_index` int(5) NOT NULL,
   `sign` varbinary(255) NOT NULL,
   PRIMARY KEY (`tx_hash`,`from_index`)
 );
