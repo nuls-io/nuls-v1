acc1ba8c40f423cf1eda45e0e4d0d8cdf474578c
niels1286 <niels0519@gmail.com>
2018-02-02 09:53:32 +0800

    debug

--- core/src/main/java/io/nuls/core/event/EventManager.java
+++ core/src/main/java/io/nuls/core/event/EventManager.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2017-2018 nuls.io
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -84,11 +84,14 @@ public class EventManager {
         EventHeader header = new EventHeader();
         header.parse(new NulsByteBuffer(bytes));
         Class<? extends BaseEvent> clazz = EVENT_MAP.get(header.getModuleId() + "_" + header.getEventType());
+        if (null == clazz) {
+            return null;
+        }
         BaseEvent event = null;
         try {
             event = clazz.newInstance();
         } catch (Exception e) {
-            System.out.println(header.getModuleId()+"==="+header.getEventType());
+            System.out.println(header.getModuleId() + "===" + header.getEventType());
             Log.error(e);
             throw new NulsException(ErrorCode.DATA_PARSE_ERROR);
         }
