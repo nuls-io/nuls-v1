2ceba392703fcd7e056d0ef37f29c9b6ffa41f6a
vivi <251556768@qq.com>
2018-03-08 19:21:33 +0800

    bug check

--- rpc-impl/src/main/java/io/nuls/rpc/resources/impl/AccountResource.java
+++ rpc-impl/src/main/java/io/nuls/rpc/resources/impl/AccountResource.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2017-2018 nuls.io
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -158,13 +158,17 @@ public class AccountResource {
         return RpcResult.getSuccess().setData(dtoList);
     }
 
-    @GET
-    @Path("/prikey/{address}")
+    @POST
+    @Path("/prikey")
     @Produces(MediaType.APPLICATION_JSON)
-    public RpcResult getPrikey(@PathParam("address") String address, @QueryParam("password") String password) {
+    public RpcResult getPrikey(AccountParamForm form) {
         RpcResult result = RpcResult.getSuccess();
-        byte[] prikey = accountService.getPrivateKey(address, password);
-        result.setData(Hex.encode(prikey));
+        try {
+            byte[] prikey = accountService.getPrivateKey(form.getAddress(), form.getPassword());
+            result.setData(Hex.encode(prikey));
+        }catch (Exception e) {
+            result = RpcResult.getFailed(e.getMessage());
+        }
         return result;
     }
 
