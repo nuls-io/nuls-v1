bb55933c3d96f6ac4470801941155d1f9ad7a679
niels1286 <niels0519@gmail.com>
2018-03-08 19:42:30 +0800

    fix a bug of consensus

--- consensus-impl/src/main/java/io/nuls/consensus/entity/block/BlockHeaderChain.java
+++ consensus-impl/src/main/java/io/nuls/consensus/entity/block/BlockHeaderChain.java
@@ -64,7 +64,7 @@ public class BlockHeaderChain implements NulsCloneable {
     public boolean addHeader(BlockHeader header) {
         lock.lock();
         HeaderDigest hd = new HeaderDigest(header.getPreHash().getDigestHex(),header.getHeight()-1);
-        if (!headerDigestList.isEmpty() &&headerDigestList.indexOf(hd)==(headerDigestList.size()-1)) {
+        if (!headerDigestList.isEmpty() &&headerDigestList.indexOf(hd)!=(headerDigestList.size()-1)) {
             return false;
         }
         headerDigestList.add(new HeaderDigest(header.getHash().getDigestHex(), header.getHeight()));
