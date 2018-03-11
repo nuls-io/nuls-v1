06ca2a164696210296ee9a59e3d46f8adc5fe8a1
daviyang35 <daviyang35@gmail.com>
2018-03-11 16:57:14 +0800

    notify subscribe

--- notify/src/main/java/io/nuls/notify/controller/NotificationController.java
+++ notify/src/main/java/io/nuls/notify/controller/NotificationController.java
@@ -25,31 +25,52 @@
 
 package io.nuls.notify.controller;
 
+import io.nuls.core.utils.json.JSONUtils;
 import io.nuls.core.utils.log.Log;
+import io.nuls.notify.controller.handler.QueryRequestHandler;
+import io.nuls.notify.controller.handler.SubscribeRequestHandler;
+import io.nuls.notify.controller.handler.UnsubscribeRequestHandler;
 import io.nuls.notify.handler.NulsEventHandler;
 import io.nuls.notify.handler.WebSocketHandler;
 import org.java_websocket.WebSocket;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author daviyang35
  * @date 2018/2/25
  */
 public class NotificationController implements WebSocketDelegate, NulsEventDelegate {
-    private List<WebSocket> connectionsList = new ArrayList<>();
+    private final List<WebSocket> connectionsList = new ArrayList<>();
     private NulsEventHandler nulsEventHandler = new NulsEventHandler();
     private WebSocketHandler webSocketHandler;
-
+    private Map<String, RequestHandler> dispatcherMap = new HashMap<>();
     private short listenPort;
 
+    public NotificationController() {
+        this.addRequestHandler(new QueryRequestHandler());
+        this.addRequestHandler(new SubscribeRequestHandler());
+        this.addRequestHandler(new UnsubscribeRequestHandler());
+    }
+
+    private void addRequestHandler(RequestHandler handler) {
+        String method = handler.method().toLowerCase();
+        if (!dispatcherMap.containsKey(method)) {
+            dispatcherMap.put(method, handler);
+        } else {
+            Log.error("Duplicate request method handler. {} {}", method, handler.getClass().getName());
+        }
+    }
+
     public void start() {
+        nulsEventHandler.setEventDelegate(this);
         webSocketHandler = new WebSocketHandler(listenPort);
         webSocketHandler.setDelegate(this);
         webSocketHandler.start();
-        nulsEventHandler.setEventDelegate(this);
         Log.info("Notify module running on port:{}", listenPort);
     }
 
@@ -67,17 +88,43 @@ public class NotificationController implements WebSocketDelegate, NulsEventDeleg
 
     @Override
     public void onConnected(WebSocket sock) {
-        connectionsList.add(sock);
+        synchronized (connectionsList) {
+            connectionsList.add(sock);
+        }
     }
 
     @Override
     public void onDisconnected(WebSocket sock) {
-        connectionsList.remove(sock);
+        synchronized (connectionsList) {
+            connectionsList.remove(sock);
+        }
     }
 
     @Override
     public void onMessage(WebSocket sock, String msg) {
+        // decode request
+        Map<String, Object> request = null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            request = JSONUtils.json2map(msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.put("status", 400);
+        }
 
+        // dispatch handler
+        if (request != null) {
+            this.handleRequest(request, response);
+        }
+
+        // encode response
+        try {
+            String resp = JSONUtils.obj2json(response);
+            sock.send(resp);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.debug("Serialized response error.");
+        }
     }
 
     public NulsEventHandler getNulsEventHandler() {
@@ -94,6 +141,23 @@ public class NotificationController implements WebSocketDelegate, NulsEventDeleg
 
     @Override
     public void onEventFire() {
-        
+
+    }
+
+    private void handleRequest(Map<String, Object> request, Map<String, Object> response) {
+        String method = (String) request.get("method");
+        if (method == null) {
+            response.put("status", 400);
+            return;
+        }
+
+        method = method.toLowerCase();
+
+        if (dispatcherMap.containsKey(method)) {
+            RequestHandler handler = dispatcherMap.get(method);
+            handler.handleRequest(request, response);
+        } else {
+            response.put("status", 404);
+        }
     }
 }
