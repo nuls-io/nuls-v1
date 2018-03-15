1da911e52a70e38b3a8a16d2d902e4e6513a9d94
vivi <251556768@qq.com>
2018-03-15 18:24:59 +0800

    correct utxo bug

--- rpc-impl/src/main/java/io/nuls/rpc/resources/impl/TransactionResource.java
+++ rpc-impl/src/main/java/io/nuls/rpc/resources/impl/TransactionResource.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2017-2018 nuls.io
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,6 +29,8 @@ import io.nuls.core.chain.entity.Transaction;
 import io.nuls.core.constant.ErrorCode;
 import io.nuls.core.context.NulsContext;
 import io.nuls.core.dto.Page;
+import io.nuls.core.exception.NulsException;
+import io.nuls.core.exception.NulsRuntimeException;
 import io.nuls.core.utils.date.TimeService;
 import io.nuls.core.utils.log.Log;
 import io.nuls.core.utils.str.StringUtils;
@@ -71,8 +73,12 @@ public class TransactionResource {
                 result = RpcResult.getSuccess();
                 result.setData(new TransactionDto(tx));
             }
+        } catch (NulsRuntimeException re) {
+            Log.error(re);
+            result = new RpcResult(false, re.getCode(), re.getMessage());
         } catch (Exception e) {
-            e.printStackTrace();
+            Log.error(e);
+            result = RpcResult.getFailed(ErrorCode.SYS_UNKOWN_EXCEPTION);
         }
 
         return result;
