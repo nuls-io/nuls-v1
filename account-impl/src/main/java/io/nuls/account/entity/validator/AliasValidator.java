4afe6b3dfb82b5877ab007ac0e8c3ab17f3e3e6f
vivi <251556768@qq.com>
2018-03-21 12:00:21 +0800

    correct account module bug

--- account-impl/src/main/java/io/nuls/account/entity/validator/AliasValidator.java
+++ account-impl/src/main/java/io/nuls/account/entity/validator/AliasValidator.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2017-2018 nuls.io
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,6 +26,7 @@ package io.nuls.account.entity.validator;
 import io.nuls.account.entity.Address;
 import io.nuls.account.entity.Alias;
 import io.nuls.account.entity.tx.AliasTransaction;
+import io.nuls.core.context.NulsContext;
 import io.nuls.core.utils.str.StringUtils;
 import io.nuls.core.validate.NulsDataValidator;
 import io.nuls.core.validate.ValidateResult;
@@ -53,21 +54,24 @@ public class AliasValidator implements NulsDataValidator<AliasTransaction> {
     @Override
     public ValidateResult validate(AliasTransaction tx) {
         Alias alias = tx.getTxData();
-        if (StringUtils.isBlank(alias.getAddress()) || new Address(alias.getAddress()).getHash().length !=23 ) {
+        if (StringUtils.isBlank(alias.getAddress()) || new Address(alias.getAddress()).getHash().length != 23) {
             return ValidateResult.getFailedResult("The address format error");
         }
         if (!StringUtils.validAlias(alias.getAlias())) {
             return ValidateResult.getFailedResult("The alias is between 3 to 20 characters");
         }
 
-        AliasPo aliasPo = aliasDataService.get(alias.getAlias());
+        AliasPo aliasPo = getAliasDataService().get(alias.getAlias());
         if (aliasPo != null) {
             return ValidateResult.getFailedResult("The alias has been occupied");
         }
         return ValidateResult.getSuccessResult();
     }
 
-    public void setAliasDataService(AliasDataService aliasDataService) {
-        this.aliasDataService = aliasDataService;
+    private AliasDataService getAliasDataService() {
+        if (aliasDataService == null) {
+            aliasDataService = NulsContext.getServiceBean(AliasDataService.class);
+        }
+        return aliasDataService;
     }
 }
