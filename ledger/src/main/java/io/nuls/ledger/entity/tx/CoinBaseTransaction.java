d0287569d6bf34519b977aece5a29d2706efec42
niels1286 <niels0519@gmail.com>
2018-03-27 20:43:37 +0800

    update agent&deposit

--- ledger/src/main/java/io/nuls/ledger/entity/tx/CoinBaseTransaction.java
+++ ledger/src/main/java/io/nuls/ledger/entity/tx/CoinBaseTransaction.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2017-2018 nuls.io
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,15 +24,12 @@
 package io.nuls.ledger.entity.tx;
 
 import io.nuls.core.chain.entity.BaseNulsData;
-import io.nuls.core.constant.ErrorCode;
-import io.nuls.core.constant.NulsConstant;
 import io.nuls.core.constant.TransactionConstant;
 import io.nuls.core.exception.NulsException;
-import io.nuls.core.exception.NulsRuntimeException;
-import io.nuls.core.utils.io.NulsByteBuffer;
+import io.nuls.core.validate.NulsDataValidator;
 import io.nuls.ledger.entity.params.CoinTransferData;
 
-import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Niels
@@ -50,10 +47,10 @@ public class CoinBaseTransaction<T extends BaseNulsData> extends AbstractCoinTra
 
     protected CoinBaseTransaction(int type, CoinTransferData params, String password) throws NulsException {
         super(type, params, password);
+
     }
 
     protected CoinBaseTransaction(int type) {
         super(type);
     }
-
 }
