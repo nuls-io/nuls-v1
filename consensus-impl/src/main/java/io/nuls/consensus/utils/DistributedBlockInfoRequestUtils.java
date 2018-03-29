f6d8e3c1e8ba1f7eb3cfd34fce0fa06940018e50
niels1286 <niels0519@gmail.com>
2018-03-29 16:41:49 +0800

    update utxo data

--- consensus-impl/src/main/java/io/nuls/consensus/utils/DistributedBlockInfoRequestUtils.java
+++ consensus-impl/src/main/java/io/nuls/consensus/utils/DistributedBlockInfoRequestUtils.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2017-2018 nuls.io
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -260,21 +260,16 @@ public class DistributedBlockInfoRequestUtils {
                 result.setBestHeight(minHeight);
                 result.setNodeIdList(nodeIds);
                 result.setFinished(true);
-                if (result.getBestHeight() < Long.MAX_VALUE) {
-                    bestBlockInfo = result;
-                } else {
-                    if (result.getBestHeight() == Long.MAX_VALUE) {
-                        result.clearHeightHashMap();
-                        Block block = NulsContext.getInstance().getBestBlock();
-                        result.setBestHeight(block.getHeader().getHeight());
-                        result.setBestHash(block.getHeader().getHash());
-                        result.putHash(result.getBestHeight(),result.getBestHash());
-                        result.setNodeIdList(this.nodeIdList);
-                    }
-
-
+                if (result.getBestHeight() == Long.MAX_VALUE) {
+                    result.clearHeightHashMap();
+                    Block block = NulsContext.getInstance().getBestBlock();
+                    result.setBestHeight(block.getHeader().getHeight());
+                    result.setBestHash(block.getHeader().getHash());
+                    result.putHash(result.getBestHeight(), result.getBestHash());
+                    result.setNodeIdList(this.nodeIdList);
                 }
-            }else if ((TimeService.currentTimeMillis() - startTime) > timeout && !(hashesMap.size() >= ((nodeIdList.size() + 1) / 2))) {
+                bestBlockInfo = result;
+            } else if ((TimeService.currentTimeMillis() - startTime) > timeout && !(hashesMap.size() >= ((nodeIdList.size() + 1) / 2))) {
                 throw new NulsRuntimeException(ErrorCode.TIME_OUT);
             }
         }
