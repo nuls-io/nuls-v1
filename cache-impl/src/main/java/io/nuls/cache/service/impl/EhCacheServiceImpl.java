ef03cee82834285529fef76d57e8147fb5785d94
niels1286 <niels0519@gmail.com>
2018-03-31 12:19:44 +0800

    update consensus

--- cache-impl/src/main/java/io/nuls/cache/service/impl/EhCacheServiceImpl.java
+++ cache-impl/src/main/java/io/nuls/cache/service/impl/EhCacheServiceImpl.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2017-2018 nuls.io
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -97,7 +97,7 @@ public class EhCacheServiceImpl<K, T> implements CacheService<K, T> {
             valueObj = ((NulsCloneable) value).copy();
         }
         if (null == cacheManager.getCache(cacheTitle)) {
-            throw new NulsRuntimeException(ErrorCode.FAILED, "Cache not exist!");
+            throw new NulsRuntimeException(ErrorCode.FAILED, "Cache not exist!2");
         }
         cacheManager.getCache(cacheTitle).put(key, valueObj);
     }
@@ -105,7 +105,7 @@ public class EhCacheServiceImpl<K, T> implements CacheService<K, T> {
     @Override
     public void putElement(CacheElement element) {
         if (null == cacheManager.getCache(element.getCacheTitle())) {
-            throw new NulsRuntimeException(ErrorCode.FAILED, "Cache not exist!");
+            throw new NulsRuntimeException(ErrorCode.FAILED, "Cache not exist!1");
         }
         cacheManager.getCache(element.getCacheTitle()).put(element.getKey(), element.getValue().copy());
     }
@@ -148,7 +148,7 @@ public class EhCacheServiceImpl<K, T> implements CacheService<K, T> {
     @Override
     public void removeElement(String cacheTitle, K key) {
         if (null == cacheManager.getCache(cacheTitle)) {
-            throw new NulsRuntimeException(ErrorCode.FAILED, "Cache not exist!");
+            return;
         }
         if (containsKey(cacheTitle, key)) {
             cacheManager.getCache(cacheTitle).remove(key);
@@ -158,7 +158,7 @@ public class EhCacheServiceImpl<K, T> implements CacheService<K, T> {
     @Override
     public void clearCache(String title) {
         if (null == cacheManager.getCache(title)) {
-            throw new NulsRuntimeException(ErrorCode.FAILED, "Cache not exist!");
+            return;
         }
         cacheManager.getCache(title).clear();
     }
@@ -170,13 +170,21 @@ public class EhCacheServiceImpl<K, T> implements CacheService<K, T> {
 
     @Override
     public boolean containsKey(String cacheTitle, K key) {
-        boolean result = this.cacheManager.getCache(cacheTitle).containsKey(key);
+        Cache cache = this.cacheManager.getCache(cacheTitle);
+        if (cache == null) {
+            return false;
+        }
+        boolean result = cache.containsKey(key);
         return result;
     }
 
     @Override
     public Set<K> keySet(String cacheTitle) {
-        Iterator it = cacheManager.getCache(cacheTitle).iterator();
+        Cache cache = this.cacheManager.getCache(cacheTitle);
+        if (null == cache) {
+            return new HashSet<>();
+        }
+        Iterator it = cache.iterator();
         Set<K> set = new HashSet<>();
         while (it.hasNext()) {
             Cache.Entry<K, T> entry = (Cache.Entry<K, T>) it.next();
