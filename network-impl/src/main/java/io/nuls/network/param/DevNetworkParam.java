b65a84c828b382f593a621db5db2a123d51aa9b8
vivi <251556768@qq.com>
2018-04-04 16:30:02 +0800

    Merge branch 'feature/networkEnhancement' into devtest

--- network-impl/src/main/java/io/nuls/network/param/DevNetworkParam.java
+++ network-impl/src/main/java/io/nuls/network/param/DevNetworkParam.java
@@ -46,7 +46,6 @@ public class DevNetworkParam extends AbstractNetworkParam {
         this.maxInCount = NetworkContext.getNetworkConfig().getPropValue(NetworkConstant.NETWORK_NODE_MAX_IN, 20);
         this.maxOutCount = NetworkContext.getNetworkConfig().getPropValue(NetworkConstant.NETWORK_NODE_MAX_OUT, 10);
         this.port = NulsContext.MODULES_CONFIG.getCfgValue(NetworkConstant.NETWORK_SECTION, NetworkConstant.NETWORK_SERVER_PORT, 8003);
-        this.externalPort = NulsContext.MODULES_CONFIG.getCfgValue(NetworkConstant.NETWORK_SECTION, NetworkConstant.NETWORK_EXTER_PORT, 8003);
         this.packetMagic = NulsContext.MODULES_CONFIG.getCfgValue(NetworkConstant.NETWORK_SECTION, NetworkConstant.NETWORK_MAGIC, 123456789);
 
 
