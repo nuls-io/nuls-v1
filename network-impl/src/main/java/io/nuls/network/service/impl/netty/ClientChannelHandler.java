00dcb1184df288ab6c96dadb79e1fee02d817f0e
vivi <251556768@qq.com>
2018-04-12 23:25:08 +0800

    network restructure

--- network-impl/src/main/java/io/nuls/network/service/impl/netty/ClientChannelHandler.java
+++ network-impl/src/main/java/io/nuls/network/service/impl/netty/ClientChannelHandler.java
@@ -114,7 +114,7 @@ public class ClientChannelHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        Log.error("--------------- ClientChannelHandler exceptionCaught :" + cause.getMessage(), cause);
+//        Log.error("--------------- ClientChannelHandler exceptionCaught :" + cause.getMessage(), cause);
         ctx.channel().close();
     }
 
