05e4afe16690e7b2e22c7e5ba19b02c19de89b1d
niels1286 <niels0519@gmail.com>
2018-04-13 01:51:26 +0800

    update consensus

--- network-impl/src/main/java/io/nuls/network/param/DevNetworkParam.java
+++ network-impl/src/main/java/io/nuls/network/param/DevNetworkParam.java
@@ -44,9 +44,9 @@ public class DevNetworkParam extends AbstractNetworkParam {
         this.port = NulsContext.MODULES_CONFIG.getCfgValue(NetworkConstant.NETWORK_SECTION, NetworkConstant.NETWORK_SERVER_PORT, 8003);
         this.packetMagic = NulsContext.MODULES_CONFIG.getCfgValue(NetworkConstant.NETWORK_SECTION, NetworkConstant.NETWORK_MAGIC, 123456789);
 
-        seedIpList.add("192.168.1.103");
-        seedIpList.add("192.168.1.102");
-        seedIpList.add("192.168.1.201");
+        seedIpList.add("120.77.241.8");
+        seedIpList.add("39.108.121");
+        seedIpList.add("101.132.33.140");
         seedIpList.add("192.168.1.233");
 
         this.messageFilter = DefaultMessageFilter.getInstance();
