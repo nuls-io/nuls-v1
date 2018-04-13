919fe93b4b5074b5a6fa315474a1629195f05f51
niels1286 <niels0519@gmail.com>
2018-04-13 20:54:20 +0800

    update consensus

--- network-impl/src/main/java/io/nuls/network/param/DevNetworkParam.java
+++ network-impl/src/main/java/io/nuls/network/param/DevNetworkParam.java
@@ -45,7 +45,7 @@ public class DevNetworkParam extends AbstractNetworkParam {
         this.packetMagic = NulsContext.MODULES_CONFIG.getCfgValue(NetworkConstant.NETWORK_SECTION, NetworkConstant.NETWORK_MAGIC, 123456789);
 
         seedIpList.add("120.77.241.8");
-        seedIpList.add("39.108.121");
+        seedIpList.add("39.108.121.242");
         seedIpList.add("101.132.33.140");
         seedIpList.add("192.168.1.103");
 
