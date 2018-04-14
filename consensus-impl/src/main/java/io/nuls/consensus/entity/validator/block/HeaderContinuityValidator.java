bb90b476f95f56afff3da0f2ff3593f2123dc9b9
niels1286 <niels0519@gmail.com>
2018-04-14 21:42:59 +0800

    update consensus

--- consensus-impl/src/main/java/io/nuls/consensus/entity/validator/block/HeaderContinuityValidator.java
+++ consensus-impl/src/main/java/io/nuls/consensus/entity/validator/block/HeaderContinuityValidator.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2017-2018 nuls.io
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,6 +29,7 @@ import io.nuls.consensus.service.intf.BlockService;
 import io.nuls.core.chain.entity.BlockHeader;
 import io.nuls.core.chain.entity.NulsDigestData;
 import io.nuls.core.constant.ErrorCode;
+import io.nuls.core.constant.SeverityLevelEnum;
 import io.nuls.core.context.NulsContext;
 import io.nuls.core.exception.NulsException;
 import io.nuls.core.utils.log.BlockLog;
@@ -65,7 +66,7 @@ public class HeaderContinuityValidator implements NulsDataValidator<BlockHeader>
                 preHeader = NulsContext.getServiceBean(BlockService.class).getBlockHeader(header.getPreHash().getDigestHex());
             } catch (NulsException e) {
                 //todo
-               Log.error(e);
+                Log.error(e);
             }
             if (null == preHeader) {
                 return ValidateResult.getFailedResult(ErrorCode.ORPHAN_BLOCK);
@@ -76,13 +77,17 @@ public class HeaderContinuityValidator implements NulsDataValidator<BlockHeader>
             } catch (NulsException e) {
                 Log.error(e);
             }
-            long shouldTime = roundData.getRoundStartTime() + roundData.getPackingIndexOfRound() * PocConsensusConstant.BLOCK_TIME_INTERVAL_SECOND*1000;
+            BlockRoundData preBlockRoundData = new BlockRoundData(preHeader.getExtend());
+            if (preBlockRoundData.getRoundIndex() == roundData.getRoundIndex() && preBlockRoundData.getPackingIndexOfRound() >= roundData.getPackingIndexOfRound()) {
+                return ValidateResult.getFailedResult(SeverityLevelEnum.FLAGRANT_FOUL, "the packing index of round is wrong");
+            }
+            long shouldTime = roundData.getRoundStartTime() + roundData.getPackingIndexOfRound() * PocConsensusConstant.BLOCK_TIME_INTERVAL_SECOND * 1000;
             //todo 3 seconds error
             long difference = header.getTime() - shouldTime;
-            long timeout = PocConsensusConstant.BLOCK_TIME_INTERVAL_SECOND*1000/2;
+            long timeout = PocConsensusConstant.BLOCK_TIME_INTERVAL_SECOND * 1000 / 2;
             failed = difference > timeout || difference < -timeout;
             if (failed) {
-                BlockLog.debug("header validate failed:"+header.getHeight()+" , time difference："+difference);
+                BlockLog.debug("header validate failed:" + header.getHeight() + " , time difference：" + difference);
                 break;
             }
         } while (false);
