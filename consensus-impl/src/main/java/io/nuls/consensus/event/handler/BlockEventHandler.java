bab5ca80434fd3840548a35ea7dd51be3dce197d
niels1286 <niels0519@gmail.com>
2018-04-15 01:51:17 +0800

    update consensus

--- consensus-impl/src/main/java/io/nuls/consensus/event/handler/BlockEventHandler.java
+++ consensus-impl/src/main/java/io/nuls/consensus/event/handler/BlockEventHandler.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2017-2018 nuls.io
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -34,6 +34,7 @@ import io.nuls.core.chain.entity.Transaction;
 import io.nuls.core.context.NulsContext;
 import io.nuls.core.utils.log.Log;
 import io.nuls.event.bus.handler.AbstractEventHandler;
+import io.nuls.ledger.entity.tx.AbstractCoinTransaction;
 import io.nuls.network.service.NetworkService;
 
 /**
@@ -66,8 +67,11 @@ public class BlockEventHandler extends AbstractEventHandler<BlockEvent> {
             if (null == cachedTx) {
                 cachedTx = OrphanTxCacheManager.getInstance().getTx(tx.getHash());
             }
-            if(cachedTx!=null&&cachedTx.getStatus()!=tx.getStatus()){
+            if (cachedTx != null && cachedTx.getStatus() != tx.getStatus()) {
                 tx.setStatus(cachedTx.getStatus());
+                AbstractCoinTransaction coinTx = (AbstractCoinTransaction) tx;
+                AbstractCoinTransaction cachedCoinTx = (AbstractCoinTransaction) cachedTx;
+                coinTx.setCoinData(cachedCoinTx.getCoinData());
 //                Log.error("the transaction status is wrong!");
             }
         }
