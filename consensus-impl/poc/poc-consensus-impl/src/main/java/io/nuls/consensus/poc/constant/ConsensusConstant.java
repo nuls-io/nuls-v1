2df59636d756f66f71a0e8dfd423cafbb3f44ce2
niels1286 <niels0519@gmail.com>
2018-04-16 18:20:09 +0800

    update block service

--- consensus-impl/poc/poc-consensus-impl/src/main/java/io/nuls/consensus/poc/constant/ConsensusConstant.java
+++ consensus-impl/poc/poc-consensus-impl/src/main/java/io/nuls/consensus/poc/constant/ConsensusConstant.java
@@ -65,7 +65,7 @@ public interface ConsensusConstant {
     /**
      * unit:second
      */
-    int BLOCK_TIME_INTERVAL_SECOND = 5;
+    int BLOCK_TIME_INTERVAL_SECOND = 2;
 
     /**
      * default:2M
--- protocol-impl/protocol-base/src/main/java/io/nuls/protocol/base/constant/PocConsensusConstant.java
+++ protocol-impl/protocol-base/src/main/java/io/nuls/protocol/base/constant/PocConsensusConstant.java
@@ -65,7 +65,7 @@ public interface PocConsensusConstant {
     /**
      * unit:second
      */
-    int BLOCK_TIME_INTERVAL_SECOND = 5;
+    int BLOCK_TIME_INTERVAL_SECOND = 2;
 
     /**
      * default:2M
