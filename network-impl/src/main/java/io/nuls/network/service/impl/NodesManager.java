6b897130e8fe210690c85fe7257e6fe9a940478a
MIMIEYES <pierreluohao@gmail.com>
2018-04-19 17:37:10 +0800

    init

--- network-impl/src/main/java/io/nuls/network/service/impl/NodesManager.java
+++ network-impl/src/main/java/io/nuls/network/service/impl/NodesManager.java
@@ -32,14 +32,12 @@ import io.nuls.core.utils.network.IpUtil;
 import io.nuls.core.utils.str.StringUtils;
 import io.nuls.db.dao.NodeDataService;
 import io.nuls.db.entity.NodePo;
-import io.nuls.db.service.intf.KVStorageService;
 import io.nuls.network.constant.NetworkConstant;
 import io.nuls.network.entity.Node;
 import io.nuls.network.entity.NodeGroup;
 import io.nuls.network.entity.NodeTransferTool;
 import io.nuls.network.entity.param.NetworkParam;
 import io.nuls.network.message.entity.VersionEvent;
-import io.nuls.network.service.NetworkService;
 import io.nuls.network.service.impl.netty.NioChannelMap;
 import io.nuls.protocol.context.NulsContext;
 
