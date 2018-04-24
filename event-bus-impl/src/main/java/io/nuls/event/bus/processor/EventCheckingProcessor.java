b72e3d280d0cca6cdceccc4e96648293beebb9d4
niels1286 <niels0519@gmail.com>
2018-04-25 00:49:06 +0800

    verify coindata&&txdata

--- event-bus-impl/src/main/java/io/nuls/event/bus/processor/EventCheckingProcessor.java
+++ event-bus-impl/src/main/java/io/nuls/event/bus/processor/EventCheckingProcessor.java
@@ -23,14 +23,17 @@ public class EventCheckingProcessor<E extends BaseEvent> implements EventHandler
         if (null == event || event.getHeader() == null) {
             return;
         }
+        String eventHash = event.getHash().getDigestHex();
         if (event.getHeader().getEventType() != EventConstant.EVENT_TYPE_COMMON_EVENT_HASH_EVENT ||
                 event.getHeader().getModuleId() != NulsConstant.MODULE_ID_EVENT_BUS) {
+            eventCacheService.cacheRecievedEventHash(eventHash);
             return;
         }
-        String eventHash = event.getHash().getDigestHex();
-        if (eventCacheService.kownTheEvent(eventHash)) {
+        CommonDigestEvent commonDigestEvent = (CommonDigestEvent) event;
+        if (eventCacheService.kownTheEvent(eventHash) || eventCacheService.kownTheEvent(commonDigestEvent.getEventBody().getDigestHex())) {
             processDataDisruptorEvent.setStoped(true);
+        } else {
+            eventCacheService.cacheRecievedEventHash(eventHash);
         }
-        eventCacheService.cacheRecievedEventHash(eventHash);
     }
 }
