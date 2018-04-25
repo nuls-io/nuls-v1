f77975ea0cfbf0fc3691220b704892c13bc3a569
vivi <251556768@qq.com>
2018-04-25 16:55:32 +0800

    ledge bug

--- ledger-impl/src/main/java/io/nuls/ledger/service/impl/UtxoCoinDataProvider.java
+++ ledger-impl/src/main/java/io/nuls/ledger/service/impl/UtxoCoinDataProvider.java
@@ -196,7 +196,7 @@ public class UtxoCoinDataProvider implements CoinDataProvider {
         Set<String> addressSet = new HashSet<>();
         lock.lock();
         try {
-            processDataInput(utxoData, spends, inputPoList, spendPoList, addressSet);
+            processDataInput(utxoData, spends, inputPoList, spendPoList, addressSet, tx);
 
             refreshCache(spends, utxoData.getOutputs());
 
@@ -249,7 +249,7 @@ public class UtxoCoinDataProvider implements CoinDataProvider {
 
     //Check if the input referenced output has been spent
     private void processDataInput(UtxoData utxoData, List<UtxoOutput> spends, List<UtxoInputPo> inputPoList,
-                                  List<UtxoOutputPo> spendPoList, Set<String> addressSet) {
+                                  List<UtxoOutputPo> spendPoList, Set<String> addressSet, Transaction tx) {
         Map<String, Object> map = new HashMap<>();
         for (int i = 0; i < utxoData.getInputs().size(); i++) {
             UtxoInput input = utxoData.getInputs().get(i);
@@ -265,8 +265,14 @@ public class UtxoCoinDataProvider implements CoinDataProvider {
 //                    throw new NulsRuntimeException(ErrorCode.UTXO_NOT_FOUND);
 //                }
 //                output = UtxoTransferTool.toOutput(outputPo);
-            if (!output.isUsable()) {
-                throw new NulsRuntimeException(ErrorCode.UTXO_STATUS_CHANGE);
+            if (tx.getType() == TransactionConstant.TX_TYPE_STOP_AGENT) {
+                if (output.getStatus() != OutPutStatusEnum.UTXO_CONSENSUS_LOCK) {
+                    throw new NulsRuntimeException(ErrorCode.UTXO_STATUS_CHANGE);
+                }
+            } else {
+                if (!output.isUsable()) {
+                    throw new NulsRuntimeException(ErrorCode.UTXO_STATUS_CHANGE);
+                }
             }
             output.setStatus(OutPutStatusEnum.UTXO_SPENT);
             spends.add(output);
