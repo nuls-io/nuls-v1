0e768f454bec2e0ec80a291721d76920b22efe37
MIMIEYES <pierreluohao@gmail.com>
2018-05-07 21:07:51 +0800

    change contract module id

--- contract-module/contract/src/main/java/io/nuls/contract/constant/ContractConstant.java
+++ contract-module/contract/src/main/java/io/nuls/contract/constant/ContractConstant.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2017-2018 nuls.io
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,20 +21,17 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package io.nuls.contract.module;
+package io.nuls.contract.constant;
 
-
-import io.nuls.contract.constant.ContractConstant;
 import io.nuls.kernel.constant.NulsConstant;
-import io.nuls.kernel.module.BaseModuleBootstrap;
 
 /**
  * @Desription:
  * @Author: PierreLuo
- * @Date: 2018/4/20
+ * @Date: 2018/5/7
  */
-public abstract class AbstractContractModule extends BaseModuleBootstrap {
-    public AbstractContractModule() {
-        super(ContractConstant.MODULE_ID_CONTRACT);
-    }
+public interface ContractConstant extends NulsConstant {
+
+    short MODULE_ID_CONTRACT = 4;
+
 }
