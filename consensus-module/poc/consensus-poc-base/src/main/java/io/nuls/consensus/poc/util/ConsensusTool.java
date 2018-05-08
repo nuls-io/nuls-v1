664f3178d701dc70d5b9ff3a249841d8f2994c15
ln <156755027@qq.com>
2018-05-08 18:02:25 +0800

    update consensus

---
+++ consensus-module/poc/consensus-poc-base/src/main/java/io/nuls/consensus/poc/util/ConsensusTool.java
@@ -0,0 +1,59 @@
+/*
+ * *
+ *  * MIT License
+ *  *
+ *  * Copyright (c) 2017-2018 nuls.io
+ *  *
+ *  * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  * of this software and associated documentation files (the "Software"), to deal
+ *  * in the Software without restriction, including without limitation the rights
+ *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  * copies of the Software, and to permit persons to whom the Software is
+ *  * furnished to do so, subject to the following conditions:
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in all
+ *  * copies or substantial portions of the Software.
+ *  *
+ *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  * SOFTWARE.
+ *
+ */
+package io.nuls.consensus.poc.util;
+
+import io.nuls.consensus.constant.ConsensusConstant;
+import io.nuls.kernel.model.Block;
+import io.nuls.kernel.model.NulsDigestData;
+import io.nuls.kernel.model.Transaction;
+import io.nuls.protocol.constant.ProtocolConstant;
+import io.nuls.protocol.model.SmallBlock;
+
+import java.util.*;
+
+/**
+ * @author Niels
+ * @date 2017/12/6
+ */
+public class ConsensusTool {
+
+    public static SmallBlock getSmallBlock(Block block) {
+        SmallBlock smallBlock = new SmallBlock();
+        smallBlock.setHeader(block.getHeader());
+        List<NulsDigestData> txHashList = new ArrayList<>();
+        for (Transaction tx : block.getTxs()) {
+            txHashList.add(tx.getHash());
+            if (tx.getType() == ProtocolConstant.TX_TYPE_COINBASE ||
+                    tx.getType() == ConsensusConstant.TX_TYPE_YELLOW_PUNISH ||
+                    tx.getType() == ConsensusConstant.TX_TYPE_RED_PUNISH) {
+                smallBlock.addConsensusTx(tx);
+            }
+        }
+        smallBlock.setTxHashList(txHashList);
+        return smallBlock;
+    }
+}
+
