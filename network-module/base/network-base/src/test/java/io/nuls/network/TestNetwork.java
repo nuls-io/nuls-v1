a4195239cca00b7abd96190954e212f5a4efbad3
vivi <251556768@qq.com>
2018-05-10 11:01:12 +0800

    //network module implement

--- network-module/base/network-base/src/test/java/io/nuls/network/TestNetwork.java
+++ network-module/base/network-base/src/test/java/io/nuls/network/TestNetwork.java
@@ -5,36 +5,35 @@ import io.nuls.db.module.impl.LevelDbModuleBootstrap;
 import io.nuls.kernel.MicroKernelBootstrap;
 import io.nuls.kernel.module.service.ModuleService;
 import io.nuls.network.module.impl.NettyNetworkModuleBootstrap;
+import io.protostuff.Tag;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TestNetwork {
 
-    public static void main(String[] args) {
-        Thread.currentThread().setName("Nuls");
-        try {
-            sysStart();
-        } catch (Exception e) {
-            Log.error(e);
-            System.exit(1);
+    @Before
+    public void init() {
+        MicroKernelBootstrap mk = MicroKernelBootstrap.getInstance();
+        mk.init();
+        mk.start();
+
+        LevelDbModuleBootstrap dbModuleBootstrap = new LevelDbModuleBootstrap();
+        dbModuleBootstrap.init();
+        dbModuleBootstrap.start();
+
+        NettyNetworkModuleBootstrap networkModuleBootstrap = new NettyNetworkModuleBootstrap();
+        networkModuleBootstrap.init();
+        networkModuleBootstrap.start();
+    }
+
+    @Test
+    public void testNetworkModule() {
+        while (true) {
+            try {
+                Thread.sleep(1000000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-    }
-
-    private static void sysStart() throws Exception {
-        do {
-            MicroKernelBootstrap mk = MicroKernelBootstrap.getInstance();
-            mk.init();
-            mk.start();
-
-            LevelDbModuleBootstrap dbModuleBootstrap = new LevelDbModuleBootstrap();
-            dbModuleBootstrap.init();
-            dbModuleBootstrap.start();
-
-            NettyNetworkModuleBootstrap networkModuleBootstrap = new NettyNetworkModuleBootstrap();
-            networkModuleBootstrap.init();
-            networkModuleBootstrap.start();
-        } while (false);
-    }
-
-    private static void initModules() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
-        ModuleService.getInstance().startModule("db","io.nuls.db.module.impl.MybatisDbModuleBootstrap");
     }
 }
