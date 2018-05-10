9391f141cb5afc717ef4c64d0dff1114ed93e707
niels1286 <niels0519@gmail.com>
2018-05-10 11:30:30 +0800

    update test case

---
+++ consensus-module/poc/consensus-poc-protocol/src/main/java/io/nuls/consensus/constant/PunishReasonEnum.java
@@ -0,0 +1,71 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017-2018 nuls.io
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+package io.nuls.consensus.constant;
+
+
+import io.nuls.kernel.i18n.I18nUtils;
+
+/**
+ * @author Niels
+ * @date 2017/12/28
+ */
+public enum PunishReasonEnum {
+    /**
+     * Bifurcate block chain
+     */
+    BIFURCATION((short) 1, 69980),
+
+    /**
+     * double spend
+     */
+    DOUBLE_SPEND((short) 2, 69981),;
+    private final short code;
+    private final int msgCode;
+
+    private PunishReasonEnum(short code, int msgCode) {
+        this.code = code;
+        this.msgCode = msgCode;
+    }
+
+    public String getMessage() {
+        return I18nUtils.get(this.msgCode);
+    }
+
+    public short getCode() {
+        return code;
+    }
+
+    public static PunishReasonEnum getEnum(int code) {
+        switch (code) {
+            case 1:
+                return PunishReasonEnum.BIFURCATION;
+            case 2:
+                return PunishReasonEnum.DOUBLE_SPEND;
+            default:
+                return null;
+        }
+
+    }
+}
