92ef3cc5c29d5328b83f42d5a0e8916c5b483fad
niels1286 <niels0519@gmail.com>
2018-05-12 16:39:34 +0800

    change serialize method

--- message-bus-module/base/message-bus-base/src/main/java/io/nuls/message/bus/message/CommonDigestMessage.java
+++ message-bus-module/base/message-bus-base/src/main/java/io/nuls/message/bus/message/CommonDigestMessage.java
@@ -1,6 +1,8 @@
 package io.nuls.message.bus.message;
 
+import io.nuls.kernel.exception.NulsException;
 import io.nuls.kernel.model.NulsDigestData;
+import io.nuls.kernel.utils.NulsByteBuffer;
 import io.nuls.message.bus.constant.MessageBusConstant;
 import io.nuls.protocol.message.base.BaseMessage;
 
@@ -11,6 +13,11 @@ import io.nuls.protocol.message.base.BaseMessage;
 public class CommonDigestMessage extends BaseMessage<NulsDigestData> {
     public CommonDigestMessage() {
         super(MessageBusConstant.MODULE_ID_MESSAGE_BUS, MessageBusConstant.MSG_TYPE_COMMON_MSG_HASH_MSG);
+    }
+
+    @Override
+    protected NulsDigestData parseMessageBody(NulsByteBuffer byteBuffer) throws NulsException {
+        return byteBuffer.readHash();
     }
 
     public CommonDigestMessage(NulsDigestData hash) {
