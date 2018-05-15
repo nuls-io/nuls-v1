591a89fa09d7449f99bda60a2b9bcb5f27f5edd6
niels1286 <niels0519@gmail.com>
2018-05-15 22:22:11 +0800

    fix Spelling mistakes

--- protocol-module/base/protocol-base/src/main/java/io/nuls/protocol/base/download/processor/DownloadProcessor.java
+++ protocol-module/base/protocol-base/src/main/java/io/nuls/protocol/base/download/processor/DownloadProcessor.java
@@ -201,10 +201,9 @@ public class DownloadProcessor extends Thread {
         Map<NulsDigestData, Integer> statisticsMaps = new HashMap<>();
         Map<NulsDigestData, List<Node>> nodeMaps = new HashMap<>();
 
+        System.out.println("--------------start download-------------------");
         for (Node node : nodeList) {
-            System.out.println("--------------");
             System.out.println(node.getId() + " : " + node.getBestBlockHeight() + " : " + node.getBestBlockHash());
-            System.out.println("--------------");
             NulsDigestData hash = node.getBestBlockHash();
 
             Integer statistics = statisticsMaps.get(hash);
