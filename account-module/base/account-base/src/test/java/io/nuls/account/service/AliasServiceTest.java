47cffb1bd6b1dc0aab1cdcba95e4bd555e035d14
Charlie <29900478@qq.com>
2018-05-18 21:27:14 +0800

    update

---
+++ account-module/base/account-base/src/test/java/io/nuls/account/service/AliasServiceTest.java
@@ -0,0 +1,28 @@
+package io.nuls.account.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AliasServiceTest {
+
+    @Test
+    public void setAlias() {
+    }
+
+    @Test
+    public void saveAlias() {
+    }
+
+    @Test
+    public void getAlias() {
+    }
+
+    @Test
+    public void isAliasExist() {
+    }
+
+    @Test
+    public void rollbackAlias() {
+    }
+}
