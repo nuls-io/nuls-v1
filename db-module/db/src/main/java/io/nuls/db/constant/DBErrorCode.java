42b18d89dbf5c43ff0b473a837f05d1b6fe7b083
MIMIEYES <pierreluohao@gmail.com>
2018-05-20 15:46:19 +0800

    add create dbbatch validation

--- db-module/db/src/main/java/io/nuls/db/constant/DBErrorCode.java
+++ db-module/db/src/main/java/io/nuls/db/constant/DBErrorCode.java
@@ -27,13 +27,14 @@
 package io.nuls.db.constant;
 
 import io.nuls.kernel.constant.ErrorCode;
+import io.nuls.kernel.constant.KernelErrorCode;
 
 /**
  * @desription:
  * @author: PierreLuo
  * @date: 2018/5/9
  */
-public interface DBErrorCode {
+public interface DBErrorCode extends KernelErrorCode {
 
     ErrorCode DB_AREA_EXIST = ErrorCode.init("DB001", 20009);
     ErrorCode DB_AREA_NOT_EXIST = ErrorCode.init("DB002", 20010);
@@ -42,4 +43,5 @@ public interface DBErrorCode {
     ErrorCode DB_AREA_CREATE_ERROR = ErrorCode.init("DB004", 20012);
     ErrorCode DB_AREA_CREATE_PATH_ERROR = ErrorCode.init("DB005", 20013);
     ErrorCode DB_AREA_DESTROY_ERROR = ErrorCode.init("DB006", 20014);
+    ErrorCode DB_BATCH_CLOSE = ErrorCode.init("DB007", 20015);
 }
