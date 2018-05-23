acc6be9ee41487ae9fe9419b6c367270aa64e400
vivi <251556768@qq.com>
2018-05-23 11:29:07 +0800

    account ledger

---
+++ account-ledger-module/base/account-ledger-base/src/main/java/io/nuls/account/ledger/base/task/CheckUnConfirmTxThread.java
@@ -0,0 +1,10 @@
+package io.nuls.account.ledger.base.task;
+
+
+public class CheckUnConfirmTxThread implements Runnable {
+    @Override
+    public void run() {
+        // todo auto-generated method stub
+
+    }
+}
