717082a72eb59eb25ca9329b232c196bd79000ed
niels1286 <niels0519@gmail.com>
2018-05-26 11:52:55 +0800

    update message bus method

---
+++ consensus-module/poc/consensus-poc-rpc/src/main/java/io/nuls/consensus/poc/rpc/model/GetCreateAgentFeeForm.java
@@ -0,0 +1,125 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017-2018 nuls.io
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package io.nuls.consensus.poc.rpc.model;
+
+import io.nuls.core.tools.str.StringUtils;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * @author Niels
+ * @date 2018/3/14
+ */
+@ApiModel(value = "创建共识(代理)节点表单数据")
+public class GetCreateAgentFeeForm {
+
+    @ApiModelProperty(name = "agentAddress", value = "申请账户的地址", required = true)
+    private String agentAddress;
+
+    @ApiModelProperty(name = "packingAddress", value = "打包地址", required = true)
+    private String packingAddress;
+    @ApiModelProperty(name = "rewardAddress", value = "结算地址", required = true)
+    private String rewardAddress;
+
+    @ApiModelProperty(name = "commissionRate", value = "佣金比例", required = true)
+    private double commissionRate;
+
+    @ApiModelProperty(name = "deposit", value = "参与共识需要的总金额", required = true)
+    private long deposit;
+
+    @ApiModelProperty(name = "remark", value = "节点备注", required = true)
+    private String remark;
+
+    @ApiModelProperty(name = "agentName", value = "节点名称", required = true)
+    private String agentName;
+
+    @ApiModelProperty(name = "password", value = "密码", required = true)
+    private String password;
+
+    public double getCommissionRate() {
+        return commissionRate;
+    }
+
+    public void setCommissionRate(double commissionRate) {
+        this.commissionRate = commissionRate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = StringUtils.formatStringPara(password);
+    }
+
+    public String getAgentAddress() {
+        return agentAddress;
+    }
+
+    public void setAgentAddress(String agentAddress) {
+        this.agentAddress = StringUtils.formatStringPara(agentAddress);
+    }
+
+    public String getPackingAddress() {
+        return packingAddress;
+    }
+
+    public void setPackingAddress(String packingAddress) {
+        this.packingAddress = StringUtils.formatStringPara(packingAddress);
+    }
+
+    public String getRewardAddress() {
+        return rewardAddress;
+    }
+
+    public void setRewardAddress(String rewardAddress) {
+        this.rewardAddress = rewardAddress;
+    }
+
+    public long getDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(long deposit) {
+        this.deposit = deposit;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = StringUtils.formatStringPara(remark);
+    }
+
+    public String getAgentName() {
+        return agentName;
+    }
+
+    public void setAgentName(String agentName) {
+        this.agentName = StringUtils.formatStringPara(agentName);
+    }
+}
