38b092f7c8e7dae33bf7504703bf230c002fdea8
niels1286 <niels0519@gmail.com>
2018-05-30 13:52:31 +0800

    update transaction hash method

--- core-module/kernel/src/main/java/io/nuls/kernel/model/Transaction.java
+++ core-module/kernel/src/main/java/io/nuls/kernel/model/Transaction.java
@@ -93,7 +93,7 @@ public abstract class Transaction<T extends TransactionLogicData> extends BaseNu
         txData = this.parseTxData(byteBuffer);
         this.coinData = byteBuffer.readNulsData(new CoinData());
         try {
-            hash = NulsDigestData.calcDigestData(this.serialize());
+            hash = NulsDigestData.calcDigestData(this.serializeForHash());
         } catch (IOException e) {
             Log.error(e);
         }
