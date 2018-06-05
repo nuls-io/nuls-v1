0a6af56419c913f3cb043229735aab1bf3ddf37c
niels1286 <niels0519@gmail.com>
2018-06-05 11:28:24 +0800

    update version

--- client-module/client/src/test/java/io/nuls/license/FileScanUtils.java
+++ client-module/client/src/test/java/io/nuls/license/FileScanUtils.java
@@ -58,12 +58,12 @@ public class FileScanUtils {
         try {
             String content = StringFileLoader.readRealPath(javaFile.getPath(), true);
             if (content.startsWith("package io.nuls")) {
-                String newContent = getMitLicense()+"\n"+content;
+                String newContent = getMitLicense() + "\n" + content;
                 FileWriter writer;
-                    writer = new FileWriter(javaFile.getPath());
-                    writer.write(newContent);
-                    writer.flush();
-                    writer.close();
+                writer = new FileWriter(javaFile.getPath());
+                writer.write(newContent);
+                writer.flush();
+                writer.close();
 
             }
         } catch (Exception e) {
@@ -74,28 +74,51 @@ public class FileScanUtils {
     private static String getMitLicense() {
         StringBuilder str = new StringBuilder();
         str.append("/*");
+        str.append("\n");
         str.append(" * MIT License");
+        str.append("\n");
         str.append(" *");
+        str.append("\n");
         str.append(" * Copyright (c) 2017-2018 nuls.io");
+        str.append("\n");
         str.append(" *");
+        str.append("\n");
         str.append(" * Permission is hereby granted, free of charge, to any person obtaining a copy");
+        str.append("\n");
         str.append(" * of this software and associated documentation files (the \"Software\"), to deal");
+        str.append("\n");
         str.append(" * in the Software without restriction, including without limitation the rights");
+        str.append("\n");
         str.append(" * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell");
+        str.append("\n");
         str.append(" * copies of the Software, and to permit persons to whom the Software is");
+        str.append("\n");
         str.append(" * furnished to do so, subject to the following conditions:");
+        str.append("\n");
         str.append(" *");
+        str.append("\n");
         str.append(" * The above copyright notice and this permission notice shall be included in all");
+        str.append("\n");
         str.append(" * copies or substantial portions of the Software.");
+        str.append("\n");
         str.append(" *");
+        str.append("\n");
         str.append(" * THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR");
+        str.append("\n");
         str.append(" * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,");
+        str.append("\n");
         str.append(" * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE");
+        str.append("\n");
         str.append(" * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER");
+        str.append("\n");
         str.append(" * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,");
+        str.append("\n");
         str.append(" * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE");
+        str.append("\n");
         str.append(" * SOFTWARE.");
+        str.append("\n");
         str.append(" *");
+        str.append("\n");
         str.append(" */");
         return str.toString();
     }
