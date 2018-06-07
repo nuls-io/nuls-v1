08a0d1cb230bc13299c27b2ee654c5a7e71dc2db
ln <ac@nuls.io>
2018-06-07 17:48:10 +0800

    update local account legder of utxo key

--- account-ledger-module/base/account-ledger-base/src/main/java/io/nuls/account/ledger/base/task/CheckUnConfirmTxThread.java
+++ account-ledger-module/base/account-ledger-base/src/main/java/io/nuls/account/ledger/base/task/CheckUnConfirmTxThread.java
@@ -148,39 +148,36 @@ public class CheckUnConfirmTxThread implements Runnable {
                 if (sourceTx == null) {
                     continue;
                 }
-                byte[] address = sourceTx.getCoinData().getTo().get((int) new VarInt(fromIndex, 0).value).getOwner();
                 try {
-                    fromMap.put(org.spongycastle.util.Arrays.concatenate(address, from.getOwner()), sourceTx.getCoinData().getTo().get((int) new VarInt(fromIndex, 0).value).serialize());
+                    Coin fromCoin = sourceTx.getCoinData().getTo().get((int) new VarInt(fromIndex, 0).value);
+
+                    if (!AccountLegerUtils.isLocalAccount(fromCoin.getOwner())) {
+                        continue;
+                    }
+
+                    fromMap.put(from.getOwner(), fromCoin.serialize());
                 } catch (IOException e) {
                     throw new NulsRuntimeException(e);
                 }
             }
-            localUtxoStorageService.batchSaveUTXO(fromMap);
 
             // delete utxo - to
             List<Coin> tos = coinData.getTo();
-            byte[] indexBytes;
             Set<byte[]> toSet = new HashSet<>();
             for (int i = 0, length = tos.size(); i < length; i++) {
                 try {
-                    byte[] outKey = org.spongycastle.util.Arrays.concatenate(tos.get(i).getOwner(), tx.getHash().serialize(), new VarInt(i).encode());
+                    Coin toCoin = tos.get(i);
+                    if (!AccountLegerUtils.isLocalAccount(toCoin.getOwner())) {
+                        continue;
+                    }
+                    byte[] outKey = org.spongycastle.util.Arrays.concatenate(tx.getHash().serialize(), new VarInt(i).encode());
                     toSet.add(outKey);
                 } catch (IOException e) {
+                    Log.info("delete unconfirmed output error");
                     throw new NulsRuntimeException(e);
                 }
             }
-            localUtxoStorageService.batchDeleteUTXO(toSet);
-        }
-
-        List<Coin> tos = tx.getCoinData().getTo();
-        for (int i = 0; i < tos.size(); i++) {
-            Coin to = tos.get(i);
-            try {
-                byte[] outKey = org.spongycastle.util.Arrays.concatenate(to.getOwner(), tx.getHash().serialize(), new VarInt(i).encode());
-                localUtxoStorageService.deleteUTXO(outKey);
-            } catch (IOException e) {
-                Log.info("delete unconfirmed output error");
-            }
+            localUtxoStorageService.batchSaveAndDeleteUTXO(fromMap, toSet);
         }
     }
 
