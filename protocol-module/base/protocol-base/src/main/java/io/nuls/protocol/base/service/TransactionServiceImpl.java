a683c91a41f2a627e75f0d9e016064933fe5418b
niels1286 <niels0519@gmail.com>
2018-06-27 21:46:11 +0800

    update forward small block

--- protocol-module/base/protocol-base/src/main/java/io/nuls/protocol/base/service/TransactionServiceImpl.java
+++ protocol-module/base/protocol-base/src/main/java/io/nuls/protocol/base/service/TransactionServiceImpl.java
@@ -129,6 +129,7 @@ public class TransactionServiceImpl implements TransactionService {
      */
     @Override
     public Result forwardTx(Transaction tx, Node excludeNode) {
+        temporaryCacheManager.cacheTx(tx);
         ForwardTxMessage message = new ForwardTxMessage();
         message.setMsgBody(tx.getHash());
         return messageBusService.broadcast(message, excludeNode, true);
