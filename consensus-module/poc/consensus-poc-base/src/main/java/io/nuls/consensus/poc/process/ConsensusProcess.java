81f28fd94190dc66879af866e5a044392a2faa34
niels1286 <niels0519@gmail.com>
2018-07-08 21:18:38 +0800

    update broadcast hanlder

--- consensus-module/poc/consensus-poc-base/src/main/java/io/nuls/consensus/poc/process/ConsensusProcess.java
+++ consensus-module/poc/consensus-poc-base/src/main/java/io/nuls/consensus/poc/process/ConsensusProcess.java
@@ -264,7 +264,7 @@ public class ConsensusProcess {
     private void broadcastSmallBlock(Block block) {
         SmallBlock smallBlock = ConsensusTool.getSmallBlock(block);
         temporaryCacheManager.cacheSmallBlock(smallBlock);
-        blockService.forwardBlock(block.getHeader().getHash(), null);
+        blockService.broadcastBlock(smallBlock);
     }
 
     private Block doPacking(MeetingMember self, MeetingRound round) throws NulsException, IOException {
