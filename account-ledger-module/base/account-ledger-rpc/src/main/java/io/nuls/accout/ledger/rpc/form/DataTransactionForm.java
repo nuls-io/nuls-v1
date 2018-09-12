c6c6e8ae5e63406afa7e97bf3c24f6b67e1f0748
vivi <251556768@qq.com>
2018-09-12 19:03:22 +0800

    // debug dapp transaction and add dapp to nuls-version.xml

--- account-ledger-module/base/account-ledger-rpc/src/main/java/io/nuls/accout/ledger/rpc/form/DataTransactionForm.java
+++ account-ledger-module/base/account-ledger-rpc/src/main/java/io/nuls/accout/ledger/rpc/form/DataTransactionForm.java
@@ -4,19 +4,19 @@ package io.nuls.accout.ledger.rpc.form;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
-@ApiModel(value = "创建零钱汇整交易")
+@ApiModel(value = "创建DAPP交易")
 public class DataTransactionForm {
 
     @ApiModelProperty(name = "address", value = "交易输入", required = true)
     private String address;
 
-    @ApiModelProperty(name = "password", value = "密码")
+    @ApiModelProperty(name = "password", value = "密码", required = false)
     private String password;
 
-    @ApiModelProperty(name = "data", value = "密码")
+    @ApiModelProperty(name = "data", value = "负载信息", required = true)
     private String data;
 
-    @ApiModelProperty(name = "remark", value = "备注")
+    @ApiModelProperty(name = "remark", value = "备注", required = false)
     private String remark;
 
     public String getPassword() {
