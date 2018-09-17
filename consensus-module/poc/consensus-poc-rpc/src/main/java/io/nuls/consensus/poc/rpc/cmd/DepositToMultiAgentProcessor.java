02ed02ee948fd697a80ce0f5140485a6fa242d2e
tanggang@nuls.io <123568TAG>
2018-09-17 21:06:00 +0800

    mutilAccountTransfer Calculation fee cmd create

--- consensus-module/poc/consensus-poc-rpc/src/main/java/io/nuls/consensus/poc/rpc/cmd/DepositToMultiAgentProcessor.java
+++ consensus-module/poc/consensus-poc-rpc/src/main/java/io/nuls/consensus/poc/rpc/cmd/DepositToMultiAgentProcessor.java
@@ -1,13 +1,24 @@
 package io.nuls.consensus.poc.rpc.cmd;
 
+import io.nuls.core.tools.str.StringUtils;
 import io.nuls.kernel.model.CommandResult;
+import io.nuls.kernel.model.Na;
+import io.nuls.kernel.model.NulsDigestData;
+import io.nuls.kernel.model.RpcClientResult;
 import io.nuls.kernel.processor.CommandProcessor;
 import io.nuls.kernel.utils.CommandBuilder;
+import io.nuls.kernel.utils.CommandHelper;
+import io.nuls.kernel.utils.RestFulUtils;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: tag
  */
 public class DepositToMultiAgentProcessor implements CommandProcessor {
+    private RestFulUtils restFul = RestFulUtils.getInstance();
     @Override
     public String getCommand() {
         return "depositToMultiAgent";
@@ -35,11 +46,55 @@ public class DepositToMultiAgentProcessor implements CommandProcessor {
 
     @Override
     public boolean argsValidate(String[] args) {
-        return false;
+        int length = args.length;
+        if(length != 4 && length != 7){
+            return  false;
+        }
+        if (!CommandHelper.checkArgsIsNull(args)) {
+            return false;
+        }
+        if(!StringUtils.validAddressSimple(args[1]) || !StringUtils.validAddressSimple(args[2])){
+            return false;
+        }
+        if(length == 7){
+            if(!NulsDigestData.validHash(args[3]) || StringUtils.isBlank(args[4]) || !StringUtils.isNuls(args[4]))
+                return  false;
+            if(!StringUtils.validPubkeys(args[5],args[6]))
+                return  false;
+        }else{
+            if(args[3] == null || args[3].length() == 0)
+                return false;
+        }
+        return true;
     }
 
     @Override
     public CommandResult execute(String[] args) {
-        return null;
+        String signAddress = args[2];
+        RpcClientResult res = CommandHelper.getPassword(signAddress, restFul);
+        if(!res.isSuccess()){
+            return CommandResult.getFailed(res);
+        }
+        String password = (String)res.getData();
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("address", args[1]);
+        parameters.put("signAddress", args[2]);
+        if(args.length == 4){
+            parameters.put("txdata",args[3]);
+        }else{
+            Long amount = Na.parseNuls(args[4]).getValue();
+            parameters.put("agentHash", args[3]);
+            parameters.put("deposit", amount);
+            parameters.put("password", password);
+            String[] pubkeys = args[3].split(",");
+            parameters.put("pubkeys", Arrays.asList(pubkeys));
+            parameters.put("m",Integer.parseInt(args[5]));
+            parameters.put("txHash",Integer.parseInt(args[6]));
+        }
+        RpcClientResult result = restFul.post("/consensus/mutilDeposit", parameters);
+        if (result.isFailed()) {
+            return CommandResult.getFailed(result);
+        }
+        return CommandResult.getResult(CommandResult.dataTransformValue(result));
     }
 }
--- consensus-module/poc/consensus-poc-rpc/src/main/java/io/nuls/consensus/poc/rpc/cmd/StopMultiAgentProcessor.java
+++ consensus-module/poc/consensus-poc-rpc/src/main/java/io/nuls/consensus/poc/rpc/cmd/StopMultiAgentProcessor.java
@@ -1,13 +1,23 @@
 package io.nuls.consensus.poc.rpc.cmd;
 
+import io.nuls.core.tools.str.StringUtils;
 import io.nuls.kernel.model.CommandResult;
+import io.nuls.kernel.model.NulsDigestData;
+import io.nuls.kernel.model.RpcClientResult;
 import io.nuls.kernel.processor.CommandProcessor;
 import io.nuls.kernel.utils.CommandBuilder;
+import io.nuls.kernel.utils.CommandHelper;
+import io.nuls.kernel.utils.RestFulUtils;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: tag
  */
 public class StopMultiAgentProcessor implements CommandProcessor {
+    private RestFulUtils restFul = RestFulUtils.getInstance();
     @Override
     public String getCommand() {
         return "stopMultiAgent";
@@ -33,11 +43,48 @@ public class StopMultiAgentProcessor implements CommandProcessor {
 
     @Override
     public boolean argsValidate(String[] args) {
-        return false;
+        int length = args.length;
+        if(length != 4 && length != 5){
+            return  false;
+        }
+        if (!CommandHelper.checkArgsIsNull(args)) {
+            return false;
+        }
+        if(!StringUtils.validAddressSimple(args[1]) || !StringUtils.validAddressSimple(args[2])){
+            return false;
+        }
+        if(length == 5){
+            if(!StringUtils.validPubkeys(args[3],args[4]))
+                return  false;
+        }else{
+            if(args[3] == null || args[3].length() == 0)
+                return false;
+        }
+        return true;
     }
 
     @Override
     public CommandResult execute(String[] args) {
-        return null;
+        String signAddress = args[2];
+        RpcClientResult res = CommandHelper.getPassword(signAddress, restFul);
+        if(!res.isSuccess()){
+            return CommandResult.getFailed(res);
+        }
+        String password = (String)res.getData();
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("address", args[1]);
+        parameters.put("signAddress", args[2]);
+        if(args.length == 4){
+            parameters.put("txdata",args[3]);
+        }else{
+            String[] pubkeys = args[3].split(",");
+            parameters.put("pubkeys", Arrays.asList(pubkeys));
+            parameters.put("m",Integer.parseInt(args[4]));
+        }
+        RpcClientResult result = restFul.post("/consensus/agent/stopMutil", parameters);
+        if (result.isFailed()) {
+            return CommandResult.getFailed(result);
+        }
+        return CommandResult.getResult(CommandResult.dataTransformValue(result));
     }
 }
