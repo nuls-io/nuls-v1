34d3a42af2c1c61b242ab662be8bd3d0eda10c36
ln <yanglinggege@163.com>
2018-09-20 17:28:27 +0800

    remove the never used import

--- consensus-module/poc/consensus-poc-base/src/main/java/io/nuls/consensus/poc/task/BlockProcessTask.java
+++ consensus-module/poc/consensus-poc-base/src/main/java/io/nuls/consensus/poc/task/BlockProcessTask.java
@@ -35,14 +35,11 @@ import io.nuls.consensus.poc.provider.BlockQueueProvider;
 import io.nuls.consensus.poc.context.ConsensusStatusContext;
 import io.nuls.core.tools.log.Log;
 import io.nuls.kernel.context.NulsContext;
-import io.nuls.kernel.exception.NulsException;
-import io.nuls.kernel.func.TimeService;
 import io.nuls.kernel.model.Transaction;
 import io.nuls.ledger.service.LedgerService;
 import io.nuls.protocol.service.DownloadService;
 import io.nuls.protocol.service.TransactionService;
 
-import java.io.IOException;
 import java.util.List;
 
 /**
@@ -107,9 +104,9 @@ public class BlockProcessTask implements Runnable {
                     }
                     first = false;
                 }
-                long time = System.currentTimeMillis();
+                //long time = System.currentTimeMillis();
                 blockProcess.addBlock(blockContainer);
-//                Log.info("add 区块 " + blockContainer.getBlock().getHeader().getHeight() + " 耗时 " + (System.currentTimeMillis() - time) + " ms , tx count : " + blockContainer.getBlock().getHeader().getTxCount());
+                //Log.info("add 区块 " + blockContainer.getBlock().getHeader().getHeight() + " 耗时 " + (System.currentTimeMillis() - time) + " ms , tx count : " + blockContainer.getBlock().getHeader().getTxCount());
             } catch (Exception e) {
                 e.printStackTrace();
                 Log.error("add block fail , error : " + e.getMessage(), e);
