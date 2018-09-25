5f232dce934281664178304feedfa1c00f3a23b4
Charlie <29900478@qq.com>
2018-09-25 09:56:13 +0800

    update test case

--- account-module/base/account-base/src/test/java/io/nuls/account/service/AccountTest.java
+++ account-module/base/account-base/src/test/java/io/nuls/account/service/AccountTest.java
@@ -105,6 +105,22 @@ public class AccountTest {
             ECKey ecKey = new ECKey();
             String prikey = Hex.encode(ecKey.getPrivKeyBytes());
             ECKey ecKey2 = ECKey.fromPrivate(new BigInteger(1, Hex.decode(prikey)));
+            ECKey ecKey3 = ECKey.fromPrivate(new BigInteger(Hex.decode(prikey)));
+            if(!Arrays.equals(ecKey.getPubKey(), ecKey3.getPubKey())){
+                //1.0.1的bug, 导入私钥BigInteger少传参数, 如果不等说明问题大了。
+                System.out.println("error: " + prikey);
+                Address address1 = new Address(NulsContext.DEFAULT_CHAIN_ID, NulsContext.DEFAULT_ADDRESS_TYPE, SerializeUtils.sha256hash160(ecKey.getPubKey()));
+                Address address3 = new Address(NulsContext.DEFAULT_CHAIN_ID, NulsContext.DEFAULT_ADDRESS_TYPE, SerializeUtils.sha256hash160(ecKey3.getPubKey()));
+                System.out.println("原始地址: " + address1.getBase58());
+                System.out.println("导入后地址3: " + address3.getBase58());
+                try {
+                    System.out.println("原始ecKey: " + JSONUtils.obj2json(ecKey));
+                    System.out.println("导入后ecKey3: " + JSONUtils.obj2json(ecKey3));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
             if (!Arrays.equals(ecKey.getPubKey(), ecKey2.getPubKey())) {
                 System.out.println("error: " + prikey);
                 Address address1 = new Address(NulsContext.DEFAULT_CHAIN_ID, NulsContext.DEFAULT_ADDRESS_TYPE, SerializeUtils.sha256hash160(ecKey.getPubKey()));
