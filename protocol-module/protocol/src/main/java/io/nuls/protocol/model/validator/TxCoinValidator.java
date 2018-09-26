aaeb30415571c0cbe619e93c9ec17984632b5bae
tanggang@nuls.io <123568TAG>
2018-09-26 16:18:20 +0800

    txcoinValidator update

--- protocol-module/protocol/src/main/java/io/nuls/protocol/model/validator/TxCoinValidator.java
+++ protocol-module/protocol/src/main/java/io/nuls/protocol/model/validator/TxCoinValidator.java
@@ -5,6 +5,7 @@ import io.nuls.kernel.constant.KernelErrorCode;
 import io.nuls.kernel.context.NulsContext;
 import io.nuls.kernel.exception.NulsException;
 import io.nuls.kernel.lite.annotation.Component;
+import io.nuls.kernel.model.Address;
 import io.nuls.kernel.model.Coin;
 import io.nuls.kernel.model.Transaction;
 import io.nuls.kernel.validate.NulsDataValidator;
@@ -19,17 +20,18 @@ public class TxCoinValidator implements NulsDataValidator<Transaction> {
     @Override
     public ValidateResult validate(Transaction tx) throws NulsException {
         try {
-            if(tx.getCoinData() != null){
-                List<Coin> toList = tx.getCoinData().getTo();
-                if(toList != null && toList.size()>0){
-                    for (Coin coin:toList) {
-                        if(coin.getOwner().length == 23 && coin.getOwner()[2] == NulsContext.P2SH_ADDRESS_TYPE){
-                            return ValidateResult.getFailedResult(this.getClass().getName(), KernelErrorCode.COIN_OWNER_ERROR);
-                        }
-                    }
+            if(tx.getCoinData() == null){
+                return ValidateResult.getSuccessResult();
+            }
+            List<Coin> toList = tx.getCoinData().getTo();
+            if(toList == null || toList.size() == 0){
+                return ValidateResult.getSuccessResult();
+            }
+            for (Coin coin:toList) {
+                if(coin.getOwner().length == Address.ADDRESS_LENGTH && coin.getOwner()[2] == NulsContext.P2SH_ADDRESS_TYPE){
+                    return ValidateResult.getFailedResult(this.getClass().getName(), KernelErrorCode.COIN_OWNER_ERROR);
                 }
             }
-
         } catch (Exception e) {
             Log.error(e);
         }
