7bd5a24a6668863cfbac49de9cd8e6e0d20825f5
vivi <251556768@qq.com>
2018-10-09 15:35:15 +0800

    change delay count for test

--- account-ledger-module/base/account-ledger-rpc/src/test/java/io/nuls/account/ledger/rpc/TransferTest.java
+++ account-ledger-module/base/account-ledger-rpc/src/test/java/io/nuls/account/ledger/rpc/TransferTest.java
@@ -57,7 +57,7 @@ public class TransferTest extends BaseTest {
 //        List<String> addressList = getAddressList();
 //
 //        for (String toAddress : addressList) {
-            String address = "Nse64QEsc4yEHNfeds5Gk3rftGmCvssc";
+            String address = "Nse9XfuzZQn7jofrV5uABiNLQwFadV2K";
             String toAddress = "Nsdw4KD2ERFax2j7vL2w8NZmtkxb9aiW";//01385ef69371c8fe003d2339158333e6b383eaf7a93a38a77d022cf06024c82a
             long amount = 1001000L;
             String password = "";
