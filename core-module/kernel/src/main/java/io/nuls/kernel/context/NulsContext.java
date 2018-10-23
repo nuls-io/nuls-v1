7139c2291b866c262669e658fc1d3709b86ee947
MIMIEYES <pierreluohao@gmail.com>
2018-10-23 20:08:33 +0800

    add comment for test

--- core-module/kernel/src/main/java/io/nuls/kernel/context/NulsContext.java
+++ core-module/kernel/src/main/java/io/nuls/kernel/context/NulsContext.java
@@ -68,6 +68,7 @@ public class NulsContext {
      * and the current address begins with "Ns".8964.
      */
     public static short DEFAULT_CHAIN_ID = 261;
+    // pierre test comment out
 
     /**
      * 默认的地址类型，一条链可以包含几种地址类型，地址类型包含在地址中
