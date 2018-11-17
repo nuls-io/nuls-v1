e5cbc25f89153643e1f6cf9eb3e657ff85d44302
zhouwei1985 <251556768@qq.com>
2018-11-17 17:17:36 +0800

    remove debug log

--- account-ledger-module/base/account-ledger-rpc/src/test/java/io/nuls/account/ledger/rpc/TransferTest.java
+++ account-ledger-module/base/account-ledger-rpc/src/test/java/io/nuls/account/ledger/rpc/TransferTest.java
@@ -57,7 +57,7 @@ public class TransferTest extends BaseTest {
 //        List<String> addressList = getAddressList();
 //
 //        for (String toAddress : addressList) {
-            String address = "TTayoJ1siL3PfUbcbk1ERdYKUHx9EAzb";
+            String address = "TTarACyVheCaPdGDwPv33yEGWvB35wdD";
             String toAddress = "TTakRBwcrXvn2EfapZnKhvHujGZhrTJK";//01385ef69371c8fe003d2339158333e6b383eaf7a93a38a77d022cf06024c82a
             long amount = 1001000L;
             String password = "";
