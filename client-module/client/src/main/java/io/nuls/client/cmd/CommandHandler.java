038438d74b43a5a318d53f6629650fd3d53a5514
niels1286 <niels0519@gmail.com>
2019-01-22 20:00:22 +0800

    update configurations

--- client-module/client/src/main/java/io/nuls/client/cmd/CommandHandler.java
+++ client-module/client/src/main/java/io/nuls/client/cmd/CommandHandler.java
@@ -202,8 +202,14 @@ public class CommandHandler {
     private void sdkInit() {
         String port = null;
         try {
-            NulsConfig.MODULES_CONFIG = ConfigLoader.loadIni(NulsConstant.MODULES_CONFIG_FILE);
             NulsConfig.NULS_CONFIG = ConfigLoader.loadIni(NulsConstant.USER_CONFIG_FILE);
+
+            String mode = NulsConfig.NULS_CONFIG.getCfgValue(NulsConstant.CFG_SYSTEM_SECTION, "mode", "main");
+            if ("main".equals(mode)) {
+                NulsConfig.MODULES_CONFIG = ConfigLoader.loadIni(NulsConstant.MODULES_CONFIG_FILE);
+            } else {
+                NulsConfig.MODULES_CONFIG = ConfigLoader.loadIni(mode + "/" + NulsConstant.MODULES_CONFIG_FILE);
+            }
             port = NulsConfig.MODULES_CONFIG.getCfgValue(RpcConstant.CFG_RPC_SECTION, RpcConstant.CFG_RPC_SERVER_PORT);
             String chainId = NulsConfig.NULS_CONFIG.getCfgValue(NulsConstant.CFG_SYSTEM_SECTION, NulsConstant.CFG_SYSTEM_DEFAULT_CHAIN_ID, "8964");
             NulsContext.getInstance().setDefaultChainId(Short.parseShort(chainId));
