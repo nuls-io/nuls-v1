c7a76d9eece8d5e5453a034624e68e4ffc8062a3
vivi <251556768@qq.com>
2019-07-22 14:07:35 +0800

    add offline create multiTransferTx

--- core-module/kernel/src/main/java/io/nuls/kernel/context/NulsContext.java
+++ core-module/kernel/src/main/java/io/nuls/kernel/context/NulsContext.java
@@ -58,6 +58,10 @@ public class NulsContext {
      * 主网运行中的版本，默认为1，会根据钱包更新到的块的最新版本做修改
      */
     public static volatile Integer MAIN_NET_VERSION = 1;
+    /**
+     * 主网运行中的版本对应的区块起始高度，默认为0，会根据钱包更新到最新版本做修改
+     */
+    public static volatile Long MAIN_NET_VERSION_HEIGHT = 0L;
 
     /**
      * 切换序列化交易HASH方法的高度
